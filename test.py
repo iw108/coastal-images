817fea71f0e41eb7214bc517b4f7d73ffc1e0df5
Isaac Williams <isaacwilliams@Isaac-Williamss-MacBook-Pro.local>
2019-03-13 20:40:04 +0100

    worked out how filtering works

--- test.py
+++ test.py
@@ -3,10 +3,10 @@ from datetime import datetime
 
 import pandas as pd
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, join
 
 from argus.core import obj_to_dict, create_session
-from argus.models import Camera, Geometry
+from argus.models import Camera, Geometry, UsedGcp, Gcp
 
 
 session = create_session()
@@ -19,12 +19,16 @@ camera = session.query(Camera).filter_by(id=camera_id).first()
 
 # get camera geometries and store in dataframe
 geometries = camera.geometry.filter(Geometry.gcp_count > 5)\
-                 .order_by(Geometry.time_valid).all()
+             .order_by(Geometry.time_valid).all()
 
 df = pd.DataFrame([obj_to_dict(geom) for geom in geometries])\
-         .set_index('id').drop(columns=['camera_id'])
+     .set_index('id').drop(columns=['camera_id'])
 df['time_elapsed'] = abs(df['time_valid'] - time_start)
 
 # get coords
-id = df['time_elapsed'].idxmin()
+id = int(df['time_elapsed'].idxmin())
+
+coords = session.query(UsedGcp, Gcp).join(Gcp)\
+         .filter(UsedGcp.geometry_id==id).all()
+
 session.close()
